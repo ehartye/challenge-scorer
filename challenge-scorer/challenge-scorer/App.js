c5e194ea1e58de13675252d1eaaa9650945285f3
“Julie <julie>
2023-05-10 21:14:46 -0400

    score component

--- challenge-scorer/challenge-scorer/App.js
+++ challenge-scorer/challenge-scorer/App.js
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import Score from './Score';
+
 
 export default function App() {
   return (
@@ -9,12 +13,38 @@ export default function App() {
     </View>
   );
 }
+// This is a new Score component.
+const Score = () => {
+  const [score, setScore] = useState(0);
+
+  return (
+    <View>
+      <Text style={styles.scoreText}>Score: {score}</Text>
+    </View>
+  );
+};
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Master Piece Scorer!</Text>
+      <Score />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  scoreText: {
+    fontSize: 20,
+    marginTop: 20,
   },
 });

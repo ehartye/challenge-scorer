a7f84718b87b0d718dc0c2dcc88d60e9031fd4a5
“Julie <julie>
2023-05-10 20:42:02 -0400

    add images

---
+++ ChallengeScorer/score.js
@@ -0,0 +1,16 @@
+// Example: Score.js with state
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+
+const Score = () => {
+  const [score, setScore] = useState(0);
+
+  return (
+    <View>
+      <Text>Score: {score}</Text>
+    </View>
+  );
+};
+
+export default Score;
+
